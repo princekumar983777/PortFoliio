fa1f38d0d28900c827e3a634f8496996d9a65b6a
Prince Kumar <princekumar9837771886@gmail.com>
2026-03-25 02:42:24 +0530

    fix pydantic[email] error

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,7 +24,7 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://port-foliio-khaki.vercel.app"
+        "https://port-foliio-khaki.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
